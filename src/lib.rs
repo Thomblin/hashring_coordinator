061dbba345ab52e072860cc5632ed52ce20a39b7
Sebastian Detert <github@elygor.de>
2025-09-02 00:38:29 +0200

    docs: remove broken link

--- src/lib.rs
+++ src/lib.rs
@@ -20,9 +20,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-//! A minimal implementation of consistent hashing as described in [Consistent
-//! Hashing and Random Trees: Distributed Caching Protocols for Relieving Hot
-//! Spots on the World Wide Web](https://www.akamai.com/es/es/multimedia/documents/technical-publication/consistent-hashing-and-random-trees-distributed-caching-protocols-for-relieving-hot-spots-on-the-world-wide-web-technical-publication.pdf).
+//! A minimal implementation of consistent hashing
+//!
 //! Clients can use the `HashRing` struct to add consistent hashing to their
 //! applications. `HashRing`'s API consists of three methods: `add`, `remove`,
 //! and `get` for adding a node to the ring, removing a node from the ring, and
